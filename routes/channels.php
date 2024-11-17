5f69aa5fdaf89e5e0409e29b212028af23c528ff
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-11-18 00:23:09 +0300

    implements  real time chat with reverb

--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+// Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+Broadcast::channel('messages', function ($user) {
+// Broadcast::channel('messages.rooms.{id}', function ($user, $id) {
+    // return (int) $user->id === (int) $id;
+    return true;
+    // return (int) $user->rooms->contains($id);
 });
